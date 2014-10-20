e125aa6fc8e5fa920f80ab286b1600c9e293a1d3
Adam Chao <me@thinkaliker.tk>
2014-10-20 00:48:13 -0700

    added fork to main function area

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <string>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
 
 int main (int argc, char** argv)
 {
-	
+	int pid = fork();
+	if (pid == 0)
+	{	
+		int error = execvp(argv[0], argv);
+		if (error == -1)
+		{
+			perror("execvp");
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+		
 	return 0;
 }
