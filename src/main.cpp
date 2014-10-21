89dac8edb55b4cfa725f58dbc74b780e2649b1de
Adam Chao <me@thinkaliker.tk>
2014-10-20 23:19:46 -0700

    actual shell and tokenization implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,50 @@
+#include <errno.h>
 #include <iostream>
 #include <string>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
 #include <unistd.h>
+#include <vector>
+#include <pwd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <boost/foreach.hpp>
+#include <boost/tokenizer.hpp>
 
 using namespace std;
+using namespace boost;
+
+string shell_prompt(); //prototype for prompt function
 
 int main (int argc, char** argv)
 {
+	while(true)
+	{
+		string input;
+		vector<string> inVector;
+
+		input = shell_prompt();
+		if (input != "exit" || input != "Exit")
+		{
+			char_separator<char> sep(";|&");
+			tokenizer< char_separator<char> > tokens(input, sep);
+			string t;
+			BOOST_FOREACH(t, tokens);
+			{
+				inVector.push_back(t);
+				cout << t << endl;
+			}
+	
+			//tokenizer<> tokens(shell_prompt());
+		}
+		else
+		{
+			cout << "Goodbye." << endl;
+		}
+		
+	}
+	/*
 	int pid = fork();
 	if (pid == 0)
 	{	
@@ -23,7 +56,6 @@ int main (int argc, char** argv)
 		}
 		else
 		{
-			//main parse here
 			if (argv[0] != "exit")
 			{
 				//execute
@@ -35,6 +67,30 @@ int main (int argc, char** argv)
 	{
 		wait(NULL);
 	}
-		
+	*/	
 	return 0;
+}
+
+string shell_prompt()
+{
+	string in;
+	//TODO - error checking for getlogin and gethostname
+	//implement later
+	/*
+	char name[256];
+	int maxlen = 64;
+	if (!gethostname(name, maxlen))
+	{
+		string strname(name);
+		cout << getlogin() << "@" << name  << "$ "; //custom prompt with hostname and login name
+		cin >> in;
+	}
+	else
+	{
+		perror("gethostname"); //throw error if not found
+	}
+	*/
+	cout << "rshell$ ";
+	cin >> in;
+	return in;
 }
