fa807ba84dd0218e1bf5a6d121af0564844c8934
Adam Chao <adam@thinkaliker.com>
2014-11-11 23:15:12 -0800

    everything but recursion

--- src/ls.cpp
+++ src/ls.cpp
@@ -26,11 +26,16 @@ void check_modifiers(const char* path, const char* name, bool hidden);
 void check_dir(const char* path, vector<string>&);
 int find_index(int argc, char** argv);
 void print_files(vector<string>, int start, bool hidden);
+void a_flag(vector<string>, char** argv, int index, bool hidden);
+void l_flag(vector<string>, char** argv, int index, bool hidden);
+bool R_flag(vector<string>, char** argv, int index, bool hidden, bool list);
+bool is_dir(const char* name, char** argv, int index);
 
 int main(int argc, char** argv)
 {
 	int flags = 0;
 	vector<string> files;
+
 	
 	setlocale(LC_ALL, "en_US.UTF-8");  //set locale for time format
 
@@ -54,97 +59,44 @@ int main(int argc, char** argv)
 //TODO: check if directory name is provided, otherwise 
 
 //TODO: implement whatever this is which has something to do with hidden files
-	if (flags & FLAG_a)
+	if (flags == 1)
 	{
-//		cout << "dbg index" << index << endl;
-		const char *dirName = ".";
-		if (argv[index] == 0)
-		{
-			dirName = "./";
-		}
-		else
-		{
-			dirName = argv[index];
-		}
-		check_dir(dirName, files);
-		sort(files.begin(), files.end());
-		print_files(files, 0, true);
-		cout << endl;
-		
+		a_flag(files, argv, index, true);
 	}
 //TODO: implement the detailed list case with the drwx items using statbuf and .st_mode
-	if (flags & FLAG_l)
+	if (flags == 2)
 	{
-		//need more code here about stuff like passing in the current directory
-		int relative = 0;
-		const char *dirName = "./";
-		char *moddir = new char[sizeof(argv[index]) + 8];
-		if (argv[index] == 0)
-		{
-			dirName = "./";
-//			cout << "nname: " << dirName << endl;
-			relative = 1;
-		}
-		else if (argv[index][0] != '/')
-		{
-			const char *dir = argv[index];
-//			char *moddir = new char[sizeof(argv[index]) + 8];
-			strcpy(moddir, "./");
-			strcat(moddir, dir);
-			strcat(moddir, "\0");
-			dirName = moddir;
-//			cout << "rname: " << dirName << endl;
-			relative = 2;
-			//delete[] moddir;
-		}
-		else
-		{
-			dirName = argv[index];
-//			cout << "name: " << dirName << endl;
-			relative = false;
-		}
-//		cout << "dir: " << dirName << endl;
-		check_dir(dirName, files);
-		sort(files.begin(), files.end());
-		for (unsigned i = 0; i < files.size(); i++)
-		{
-			const char *name = files.at(i).c_str();
-			if (relative == 1)
-			{
-				check_modifiers(name, name, 0);
-			}
-			else if (relative == 2)
-			{
-				char *modname = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 32];
-				strcpy(modname, dirName);
-				strcat(modname, "/");
-				strcat(modname, name);
-				strcat(modname, "\0");
-				check_modifiers(modname, name, 0);
-				delete[] modname;
-			}
-			else
-			{
-	//			char *modname = argv[index];
-	//			modname = strcat(modname, name);
-				const char *arg = argv[index];
-				
-				char *modname = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 32];
-				//char *modarg = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 1];
-				strcpy(modname, arg);
-				strcat(modname, "/");
-				strcat(modname, name);
-				strcat(modname, "\0");
-	//				cout << "modname: " << modname << endl << flush;
-				check_modifiers(modname, name, 0);
-				delete[] modname;
-				//delete[] modarg;
-			}
-		}
-		delete[] moddir;
-		//additional info like user, machine, additional stuff
+		l_flag(files, argv, index, false);
+	}
+
+	if (flags == 3)  //a and l
+	{
+		l_flag(files, argv, index, true);
 	}
 	
+	if (flags & FLAG_R)
+	{
+		//recursive stuff
+		R_flag(files, argv, index, false, false);
+	}
+
+	if (flags == 5) //a and R
+	{
+		//recursive stuff
+		R_flag(files, argv, index, true, false);
+	}
+
+	if (flags == 6) //l and R
+	{
+		//recursive stuff
+		R_flag(files, argv, index, false, true);
+	}
+	
+	if (flags == 7) //all the flags
+	{
+		//recursive stuff
+		R_flag(files, argv, index, true, true);
+	}
 
 //TODO: implement the recursive function...ew, recursion
 
@@ -155,21 +107,9 @@ int main(int argc, char** argv)
 //TODO: create a default case where no parameters are given
 
 
-	if ((index == 1))
+	if (flags == 0)
 	{
-		const char *dirName = "./";
-		if (argv[index] == 0)
-		{
-			dirName = "./";
-		}
-		else
-		{
-			dirName = argv[index];
-		}
-
-		check_dir(dirName, files);
-		sort(files.begin(), files.end());
-		print_files(files, 0, false);
+		a_flag(files, argv, index, false);
 	}
 
 
@@ -177,6 +117,227 @@ int main(int argc, char** argv)
 	return 0;
 }
 
+void a_flag(vector<string> files, char ** argv, int index, bool hidden)
+{
+//		cout << "dbg index" << index << endl;
+	const char *dirName = ".";
+	if (argv[index] == 0)
+	{
+		dirName = "./";
+	}
+	else
+	{
+		dirName = argv[index];
+	}
+	check_dir(dirName, files);
+	sort(files.begin(), files.end());
+	print_files(files, 0, hidden);
+//1	cout << endl;
+}
+
+void l_flag(vector<string> files, char** argv, int index, bool hidden)
+{
+	//need more code here about stuff like passing in the current directory
+	int relative = 0;
+	const char *dirName = "./";
+	char *moddir = new char[sizeof(argv[index]) + 8];
+	if (argv[index] == 0)
+	{
+		dirName = "./";
+//			cout << "nname: " << dirName << endl;
+		relative = 1;
+	}
+	else if (argv[index][0] != '/')
+	{
+		const char *dir = argv[index];
+//			char *moddir = new char[sizeof(argv[index]) + 8];
+		strcpy(moddir, "./");
+		strcat(moddir, dir);
+		strcat(moddir, "\0");
+		dirName = moddir;
+//			cout << "rname: " << dirName << endl;
+		relative = 2;
+		//delete[] moddir;
+	}
+	else
+	{
+		dirName = argv[index];
+//			cout << "name: " << dirName << endl;
+		relative = false;
+	}
+//		cout << "dir: " << dirName << endl;
+	check_dir(dirName, files);
+	sort(files.begin(), files.end());
+	for (unsigned i = 0; i < files.size(); i++)
+	{
+		const char *name = files.at(i).c_str();
+		if (relative == 1)
+		{
+			check_modifiers(name, name, hidden);
+		}
+		else if (relative == 2)
+		{
+			char *modname = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 32];
+			strcpy(modname, dirName);
+			strcat(modname, "/");
+			strcat(modname, name);
+			strcat(modname, "\0");
+			check_modifiers(modname, name, hidden);
+			delete[] modname;
+		}
+		else
+		{
+//			char *modname = argv[index];
+//			modname = strcat(modname, name);
+			const char *arg = argv[index];
+			
+			char *modname = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 32];
+			//char *modarg = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 1];
+			strcpy(modname, arg);
+			strcat(modname, "/");
+			strcat(modname, name);
+			strcat(modname, "\0");
+//				cout << "modname: " << modname << endl << flush;
+			check_modifiers(modname, name, hidden);
+			delete[] modname;
+			//delete[] modarg;
+		}
+	}
+	delete[] moddir;
+	//
+	//additional info like user, machine, additional stuff
+}
+
+bool R_flag(vector<string> files, char** argv, int index, bool hidden, bool list)
+{
+/*
+	//check if its a directoriy
+	//if it is go into it and repeat
+	//if it isnt then print out whatever's in the current directory
+	int relative = 0;
+	const char *dirName = "./";
+	char *moddir = new char[sizeof(argv[index]) + 8];
+	if (argv[index] == 0)
+	{
+		dirName = "./";
+		relative = 1;
+	}
+	else if (argv[index][0] != '/')
+	{
+		const char *dir = argv[index];
+		strcpy(moddir, "./");
+		strcat(moddir, dir);
+		strcat(moddir, "\0");
+		dirName = moddir;
+		relative = 2;
+	}
+	else
+	{
+		dirName = argv[index];
+		relative = 0;
+	}
+	check_dir(dirName, files);
+	sort(files.begin(), files.end());
+	for (unsigned i = 0; i < files.size(); i++)
+
+	{
+		char *modname = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 32];
+		const char *name = files.at(i).c_str();
+		if (relative == 1)
+		{
+			if(is_dir(name, argv, index))
+			{
+				vector<string> newfiles;
+				check_dir(name, newfiles);
+				sort(newfiles.begin(), newfiles.end());
+				R_flag(newfiles, name, index, hidden, list);
+				delete[] modname;
+				return true;
+			}
+			else
+			{
+				if (hidden)
+				{
+					if(list)
+					{
+						l_flag(files, argv, index, hidden);
+					}
+					else
+					{
+						a_flag(files, argv, index, list);
+					}
+				}
+				else
+				{
+					if(list)
+					{
+						l_flag(files, argv, index, hidden);
+					}
+					else
+					{
+						a_flag(files, argv, index, list);
+					}
+				}
+					
+			}
+			//check_modifiers(name, name, hidden);
+		}
+		else if (relative == 2)
+		{
+			char *modname = new char[sizeof(files.at(i).c_str()) + sizeof(argv[index]) + 32];
+			strcpy(modname, dirName);
+			strcat(modname, "/");
+			strcat(modname, name);
+			strcat(modname, "\0");
+			if(is_dir(modname, argv, index))
+			{
+				vector<string> newfiles;
+				check_dir(modname, newfiles);
+				sort(newfiles.begin(), newfiles.end());
+				R_flag(newfiles, modname, index, hidden, list);
+				delete[] modname;
+				return true;
+			}
+			//check_modifiers(modname, name, hidden);
+		}
+		else
+		{
+			const char *arg = argv[index];
+			strcpy(modname, arg);
+			strcat(modname, "/");
+			strcat(modname, name);
+			strcat(modname, "\0");
+			if(is_dir(modname, argv, index))
+			{
+				delete[] modname;
+				return true;
+			}
+			//check_modifiers(modname, name, hidden);
+			delete[] modname;
+		}
+	}
+*/
+	return false; //gives up on recursion
+}
+
+bool is_dir(const char* name, char** argv, int index)
+{
+	struct stat statbuf;
+	errno = 0;
+	stat(name, &statbuf);
+	if (errno == 0)
+	{
+		if(S_ISDIR(statbuf.st_mode))
+			return true;
+		else
+			return false;
+	}
+	else
+	{
+		perror("stat");
+		exit(1);
+	}
+}
 
 void print_files(vector<string> files, int start, bool hidden)
 {
@@ -190,7 +351,7 @@ void print_files(vector<string> files, int start, bool hidden)
 	}
 	else
 	{	
-		for (unsigned i = 0; i < files.size(); i++)
+		for (unsigned i = start; i < files.size(); i++)
 		{
 			
 			if (files.at(i).at(0) != '.')
@@ -207,8 +368,6 @@ void print_files(vector<string> files, int start, bool hidden)
 		}
 	}
 	cout << endl;
-
-	
 }
 
 void check_modifiers(const char* name, const char* realname, bool hidden)
@@ -226,7 +385,7 @@ void check_modifiers(const char* name, const char* realname, bool hidden)
 //	cout << "name: " << name << "realname: " << realname << endl <<flush;
 //
 
-	if (realname[0] != '.')
+	if (!((realname[0] == '.') && (!hidden)))  // A implies B
 	{
 		stat(name, &statbuf);
 		if(errno == 0)
